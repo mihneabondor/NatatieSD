98a29cd1fd28d745e5eb488b9c734db789c3dd70
mihneabondor <mihnea.bondor@icloud.com>
2025-03-31 09:53:43 +0300

    date si citire

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 #include "Culoar.h"
 #include "Rata.h"
@@ -7,27 +9,39 @@
 using namespace std;
 
 int main() {
-    VectorDinamic<Rata> rate;
-    Rata r1(1, 1), r2(2, 2), r3(3, 3), r4(4, 4), r5(5, 5);
-    rate.add(r2);
-    rate.add(r1);
-    rate.add(r3);
-    rate.add(r5);
-    rate.add(r4);
+    ifstream f("/Users/mihnea/Documents/GitHub/NatatieSD/Data/01-natatie.in");
+    if (!f) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
 
-    for (int i = 0; i < rate.getSize(); i++)
-        cout << rate[i] << '\n';
-    // ratele sunt sortate automat dupa rezistenta si culoarele dupa dimensiune
-
+    int N,M;
+    f >> N >> M;
+    vector<int> viteza(N);
+    for (int i = 0; i < N; i++) {
+        f >> viteza[i];
+    }
+    vector<int> rezistenta(N);
+    for (int i = 0; i < N; i++) {
+        f >> rezistenta[i];
+    }
     VectorDinamic<Culoar> culoare;
-    Culoar c1(1), c2(2), c3(3), c4(4), c5(5);
-    culoare.add(c2);
-    culoare.add(c1);
-    culoare.add(c5);
-    culoare.add(c3);
-    culoare.add(c2);
+    for (int i = 0; i < M; i++) {
+        int x;
+        f >> x;
+        Culoar c(x);
+        culoare.add(c);
+    }
+    f.close();
+
+    VectorDinamic<Rata> rate;
+    for (int i = 0; i < N; i++) {
+        Rata r(viteza[i], rezistenta[i]);
+        rate.add(r);
+    }
 
     for (int i = 0; i < culoare.getSize(); i++)
-        cout << culoare[i] << '\n';
+        cout << culoare[i] << endl;
+
     return 0;
 }
